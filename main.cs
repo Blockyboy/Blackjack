e7327989f41fd323099587cce99b23842d0018b3
theblockyguy4 <38402882-theblockyguy4@users.noreply.replit.com>
2024-12-16 16:45:47 +0000

    Initialised the deck, along with displaying it for testing purposes.

--- main.cs
+++ main.cs
@@ -1,13 +1,54 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+
+public class Card
+{
+  public string suit;
+  public int number;
+}
 
 class Program 
 {
   public static void Main (string[] args) 
   {
+    int cardindex = 0;
+    
     Console.WriteLine ("Blackjack");
-    List<int> deck = new List<int>();
-    List<int> pcards= new List<int>();
-    List<int> dcards= new List<int>();
+    
+    List<Card> deck = new List<Card>();
+    List<Card> pcards= new List<Card>();
+    List<Card> dcards= new List<Card>();
+
+    foreach(var a in Enumerable.Range(0,52)) //Creates slots for deck
+    {
+      deck.Add(new Card());
+    }
+    
+    for (int i = 1; i < 14; i++) //Creates card (11, 12 and 13 are Jack, Queen and King)
+    {
+      deck[cardindex].suit = "Hearts";
+      deck[cardindex].number= i;
+      ++cardindex;
+      
+      deck[cardindex].suit = "Clubs";
+      deck[cardindex].number= i;
+      ++cardindex;
+      
+      deck[cardindex].suit = "Spades";
+      deck[cardindex].number= i;
+      ++cardindex;
+      
+      deck[cardindex].suit = "Diamonds";
+      deck[cardindex].number= i;
+      ++cardindex;
+    }
+
+    foreach(Card a in deck)
+    {
+      Console.Write(a.suit);
+      Console.Write(Convert.ToString(a.number));
+      Console.WriteLine("");
+    }
   }
 }
