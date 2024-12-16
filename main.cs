55940d960774e3688b3d36664fbf6e95baf16f75
theblockyguy4 <38402882-theblockyguy4@users.noreply.replit.com>
2024-12-16 15:23:44 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+class Program 
+{
+  public static void Main (string[] args) 
+  {
+    Console.WriteLine ("Blackjack");
+    List<int> deck = new List<int>();
+    List<int> pcards= new List<int>();
+    List<int> dcards= new List<int>();
+  }
+}
